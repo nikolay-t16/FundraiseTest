be9a958babbcba18981c28e0c77f5833f2ae26cd
Nikolay Tereshchenko <nikolay.ter@gmail.com>
2020-05-23 21:47:07 +0300

    Init vue

---
+++ src/components/DonateForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="donate-form">
+    <form>
+
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class DonateForm extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped lang="scss">
+  .donate-form{
+    max-width: 400px;
+  }
+</style>
